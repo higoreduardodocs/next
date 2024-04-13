706c3c24aecfcee1a5c13c49cf152899309a3aab
higoreduardodocs <higoreduardodocs@gmail.com>
2024-04-13 13:38:51 -0400

    :technologist: Refactor: edit params in router instance

--- server/src/routes/admin-route.ts
+++ server/src/routes/admin-route.ts
@@ -1,24 +1,17 @@
 import { Router } from 'express'
 
-import * as authMiddleware from '../middlewares/auth-middleware'
+import { validate } from '../middlewares/auth-middleware'
 import * as eventController from '../controllers/event-controller'
 
 const router = Router()
 
 router.get('/ping', (req, res) => res.json({ ping: true, admin: true }))
 
-router.post('/events', authMiddleware.validate, eventController.addEvent)
-router.patch(
-  '/events/:id',
-  authMiddleware.validate,
-  eventController.updateEvent
-)
-router.get('/events', authMiddleware.validate, eventController.getAllEvent)
-router.get('/events/:id', authMiddleware.validate, eventController.getOneEvent)
-router.delete(
-  '/events/:id',
-  authMiddleware.validate,
-  eventController.removeEvent
-)
+const eventsRoute = '/events'
+router.post(`${eventsRoute}`, validate, eventController.addEvent)
+router.patch(`${eventsRoute}/:id`, validate, eventController.updateEvent)
+router.get(`${eventsRoute}`, validate, eventController.getAllEvent)
+router.get(`${eventsRoute}/:id`, validate, eventController.getOneEvent)
+router.delete(`${eventsRoute}/:id`, validate, eventController.removeEvent)
 
 export default router
