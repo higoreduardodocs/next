fb47976d051a03cf45bf4aebffe005642338939f
higoreduardodocs <higoreduardodocs@gmail.com>
2024-04-21 13:04:21 -0400

    :sparkles: Feat: create profile page

---
+++ web/src/components/ui/user-card.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @next/next/no-img-element */
+
+const images = [
+  '/images/default-blue.png',
+  '/images/default-red.png',
+  '/images/default-slate.png',
+  '/images/default-green.png',
+]
+
+interface IProps {
+  name: String
+  onClick: () => void
+}
+
+const UserCard: React.FC<IProps> = ({ name, onClick }) => {
+  const imgSrc = images[Math.floor(Math.random() * 4)]
+
+  return (
+    <button
+      type="button"
+      className="group flex-row w-44 mx-auto"
+      onClick={onClick}
+    >
+      <div
+        className="
+        flex items-center justify-center
+        w-44 h-44
+        rounded-md border-2 border-transparent
+        group-hover:cursor-pointer group-hover:border-white
+        overflow-hidden
+      "
+      >
+        <img
+          src={imgSrc}
+          alt="Profile"
+          draggable={false}
+          className="w-max h-max object-contain"
+        />
+      </div>
+      <div
+        className="
+        text-2xl text-gray-400 text-center
+        mt-4 group-hover:text-white
+      "
+      >
+        {name}
+      </div>
+    </button>
+  )
+}
+export default UserCard
--- web/src/pages/profiles.tsx
+++ web/src/pages/profiles.tsx
@@ -1,5 +1,10 @@
 import { NextPageContext } from 'next'
 import { getSession } from 'next-auth/react'
+import { useCallback } from 'react'
+import { useRouter } from 'next/router'
+
+import useCurrentUser from '@/hooks/use-current-user'
+import UserCard from '@/components/ui/user-card'
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context)
@@ -19,5 +24,24 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Profiles() {
-  return <main>Profiles</main>
+  const router = useRouter()
+  const { data } = useCurrentUser()
+
+  const selectProfile = useCallback(() => {
+    router.push('/')
+  }, [router])
+
+  return (
+    <main className="flex items-center min-h-screen justify-center">
+      <div className="flex flex-col gap-8">
+        <h1 className="text-white text-3xl md:text-6xl text-center">
+          Who&#39;s watching?
+        </h1>
+
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <UserCard name={data?.currentUser?.name} onClick={selectProfile} />
+        </div>
+      </div>
+    </main>
+  )
 }
