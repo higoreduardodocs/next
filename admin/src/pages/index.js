cc4d7d32863ff9ac6648e1c4f494356b42997491
higoreduardodocs <higoreduardodocs@gmail.com>
2024-04-22 15:55:42 -0400

    :sparkles: Feat: create home page

--- admin/src/pages/index.js
+++ admin/src/pages/index.js
@@ -1,3 +1,20 @@
+import { useSession } from 'next-auth/react'
+
 export default function Home() {
-  return <main>Home</main>
+  const { data: session } = useSession()
+
+  return (
+    <main>
+      <div className="flex justify-between">
+        <h2 className="text-blue-900">
+          Hello! <b>{session?.user?.name}</b>
+        </h2>
+
+        <div className="bg-gray-300 flex items-center rounded-lg overflow-hidden">
+          <img src={session?.user?.image} alt="User" className="w-10 h-10" />
+          <span className="text-black px-1 py-2">{session?.user?.name}</span>
+        </div>
+      </div>
+    </main>
+  )
 }
