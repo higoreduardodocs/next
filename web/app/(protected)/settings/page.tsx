d57404023b39c9dbb8c20c8af7765dd0cbc7f396
higoreduardodocs <higoreduardodocs@gmail.com>
2024-08-11 19:19:35 -0400

    ✨ Feat: create user button

--- web/app/(protected)/settings/page.tsx
+++ web/app/(protected)/settings/page.tsx
@@ -1,20 +1,41 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-export default async function SettingsPage() {
-  const session = await auth();
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+export default function SettingsPage() {
+  const user = useCurrentUser();
+
+  const onClick = () => {
+    logout();
+  };
 
   return (
-    <div>
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          "use server";
-
-          await signOut();
-        }}
-      >
-        <button type="submit">Sign Out</button>
-      </form>
+    <div className="bg-white p-10 rounded-xl">
+      <button type="submit" onClick={onClick}>
+        Sign Out
+      </button>
     </div>
   );
 }
+
+// import { auth, signOut } from "@/auth";
+
+// export default async function SettingsPage() {
+//   const session = await auth();
+
+//   return (
+//     <div>
+//       {JSON.stringify(session)}
+//       <form
+//         action={async () => {
+//           "use server";
+
+//           await signOut();
+//         }}
+//       >
+//         <button type="submit">Sign Out</button>
+//       </form>
+//     </div>
+//   );
+// }
