4deaf8a3084ac95a2e04b40ac934464b5d43f8c4
higoreduardodocs <higoreduardodocs@gmail.com>
2024-08-11 20:22:43 -0400

    ✨ Feat: create admin

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "@/components/ui/sonner";
 
 import { auth } from "@/auth";
 
@@ -23,7 +24,10 @@ export default async function RootLayout({
   return (
     <SessionProvider session={session}>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Toaster />
+          {children}
+        </body>
       </html>
     </SessionProvider>
   );
